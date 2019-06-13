e5869efb171674c510ba1be187b2cbe7da3acd30
Nattaphon_nat <nattaphon_nat@PRONTOLAP0027.mshome.net>
2019-06-13 21:36:17 +0700

    write code for testcase 1

--- which_elevaluater.py
+++ which_elevaluater.py
@@ -3,11 +3,12 @@ import unittest
 class which_elevaluater(unittest.TestCase):
 
     def test_flor_1_Press_up_lift_1(self):
-        expected = 1
+        expected = 'eveluater1'
         actual = which_evaluater_down(1, 'up')
         self.assertEqual(actual, expected)
 
 def which_evaluater_down(flor, direction):
-    pass
+        if flor == 1 and direction == 'up':
+            return 'eveluater1'
 
 unittest.main()
