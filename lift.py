fae1780389da23824b84395b27fd5db45447d9b8
Nattaphon_nat <nattaphon_nat@PRONTOLAP0027.local>
2019-06-13 10:38:53 +0700

    test case lift and code for testcase v.1

--- lift.py
+++ lift.py
@@ -1,31 +1,41 @@
 import unittest
 
 class Test_Lift(unittest.TestCase):
-    global lift
+    global lift, b_lift
     lift = [(1, 2), (2, 3), (3, 5)]
 
 
-    def test_press_2_then_should_be_Lift1(self):
+    def test_flor1_then_should_be_lift1(self):
         expected = 1
-        actual = where_lift_come(2)
+        actual = where_lift_come(1)
         self.assertEqual(actual, expected)
 
-    def test_press_3_then_should_be_lift2(self):
+    def test_flor4_then_should_be_lift2(self):
+        expected = 2
+        actual = where_lift_come(4)
+        self.assertEqual(actual, expected)
+
+    def test_flor3_then_should_be_lift2(self):
         expected = 2
         actual = where_lift_come(3)
         self.assertEqual(actual, expected)
 
-    def test_press_5_then_should_be_lift3(self):
+    def test_flor_2_then_should_be_lift1(self):
+        expected = 1
+        actual = where_lift_come(2)
+        self.assertEqual(actual, expected)
+
+    def test_flor_5_then_should_be_lift3(self):
         expected = 3
         actual = where_lift_come(5)
         self.assertEqual(actual, expected)
 
 
-def where_lift_come(press):
-    for i in lift:
-        for flor in i:
-            if flor == press:
-                return i[0]
-
+def where_lift_come(st_flors):
+    if st_flors == 1 or st_flors == 2:
+        return 1
+    if st_flors ==3 or st_flors == 4:
+        return 2
+    return 3
 
 unittest.main()
